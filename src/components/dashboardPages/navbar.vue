3f89bec54c9c45f4c5678cc4c1c4784c8019c790
chunwen <hsbsbbdn@gmail.com>
2019-10-06 01:56:45 +0800

    新增登出效果

--- src/components/dashboardPages/navbar.vue
+++ src/components/dashboardPages/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">一個人的背包客旅行</a>
       <input
         class="form-control form-control-dark w-100"
         type="text"
@@ -10,14 +10,29 @@
       />
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
+          <a class="nav-link" href="#" @click.prevent="signOut">Sign out</a>
         </li>
       </ul>
     </nav>
   </div>
 </template>
 <script>
+
 export default {
-  
+  name:"navbar",
+  methods: {
+    signOut() {
+      const api = `${process.env.APIPATH}/logout`;
+      const self = this;
+      this.$http.post(api).then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+          self.$router.push("/login");
+        }else{
+          console.log('請重新登入')
+        }
+      });
+    }
+  }
 }
 </script>

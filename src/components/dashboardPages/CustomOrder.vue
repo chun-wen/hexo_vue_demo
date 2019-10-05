1ef98237083c80fd2afb62020655e5bd5d053a15
chunwen <hsbsbbdn@gmail.com>
2019-10-06 02:03:20 +0800

    新增顧客訂購頁面

---
+++ src/components/dashboardPages/CustomOrder.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <loading :active.sync="isLoading"></loading>
+    <div class="row mt-4">
+      <div class="col-md-4 mb-4" v-for="item in products" :key="item.id">
+        <div class="card border-0 shadow-sm">
+          <div
+            style="height: 150px; background-size: cover; background-position: center"
+            :style="{ backgroundImage: `url(${item.imageUrl})` }"
+          ></div>
+          <div class="card-body">
+            <span class="badge badge-secondary float-right ml-2">{{item.category}}</span>
+            <h5 class="card-title">
+              <a href="#" class="text-dark">{{item.title}}</a>
+            </h5>
+            <p class="card-text">{{item.description}}</p>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <del class="h6">原價 {{item.origin_price}}</del>
+              <div class="h5">現在只要 {{item.price}}</div>
+            </div>
+          </div>
+          <div class="card-footer d-flex">
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click="productDetail(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+              查看更多
+            </button>
+            <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 產品列表卡片modal -->
+    <div class="modal fade" id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{product.title}}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div
+            style="height: 150px; background-size: cover; background-position: center"
+            :style="{ backgroundImage: `url(${product.imageUrl})` }"
+            ></div>
+            <span class="badge badge-secondary float-right ml-2">{{product.category}}</span>
+            <h5 class="card-title">
+              <a href="#" class="text-dark">{{product.title}}</a>
+            </h5>
+            <p class="card-text">{{product.description}}</p>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <del class="h6">原價 {{product.origin_price}}</del>
+              <div class="h5">現在只要 {{product.price}}</div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <!-- <option selected> 選購一{{product.unit}}</option> -->
+              <option :value="num" v-for="num in 10" :key="num">
+                選購{{num}}{{product.unit}}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <span>小計{{product.num * product.price}}</span>
+            <button type="button" class="btn btn-primary">加到購物車</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import pagination from "../productPage/pagination";
+
+export default {
+  components: {
+    pagination
+  },
+  data() {
+    return {
+      products: [],
+      product:{},
+      isLoading: false,
+      pagination: {},
+      status:{
+        loadingItem:'',
+      }
+    };
+  },
+  methods: {
+    getProducts(page = 1) {
+      const vm = this;
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products?page=${page}`;
+      vm.isLoading = true;
+      this.$http.get(api).then(response => {
+        console.log(response.data);
+        vm.isLoading = false;
+        vm.products = response.data.products; //開啟console.log確認是否成功
+        vm.pagination = response.data.pagination; //將變數存入
+      });
+    },
+    productDetail(id) {
+      const vm = this;
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      // vm.isLoading = true;
+      vm.status.loadingItem = id;
+      this.$http.get(api).then(response => {
+        console.log(response.data);
+        $("#productModal").modal("show");
+        vm.product = response.data.product; //開啟console.log確認是否成功
+        response.data.product.num = 1; //將數量設為ㄧ
+         vm.status.loadingItem = '';
+      });
+    }
+  },
+  created() {
+    this.getProducts();
+  }
+};
+</script>

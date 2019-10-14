e49bdc3ffbcd617e7a832b6ee31087cd94ebebff
chunwen <hsbsbbdn@gmail.com>
2019-10-15 01:01:49 +0800

    新增送出訂單

--- src/components/dashboardPages/CustomOrder.vue
+++ src/components/dashboardPages/CustomOrder.vue
@@ -86,15 +86,22 @@
               <i class="far fa-trash-alt"></i>
             </button>
           </td>
-          <td>{{item.product.title}}</td>
+          <td>
+            {{item.product.title}}
+            <div class="text-success" v-if="item.coupon">已套用優惠券</div>
+          </td>
           <td>{{item.qty}}/{{ item.product.unit }}</td>
-          <td class="text-right">{{item.qty*item.product.price}}</td>
+          <td class="text-right">{{item.qty*item.product.price | currency}}</td>
         </tr>
       </tbody>
       <tfoot>
         <tr>
           <td colspan="3" class="text-right">總計</td>
-          <td class="text-right">{{ cart.total }}</td>
+          <td class="text-right">{{ cart.total | currency }}</td>
+        </tr>
+        <tr v-if="cart.final_total !== cart.total ">
+          <td colspan="3" class="text-right text-success">折扣價</td>
+          <td class="text-right text-success">{{ cart.final_total | currency }}</td>
         </tr>
       </tfoot>
     </table>
@@ -103,6 +110,50 @@
           <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="addCouponCode">套用優換券</button>
           </div>
+    </div>
+    <!-- 送出訂單表格 -->
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6" @submit.prevent="sendOrder">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input type="email" class="form-control" name="email" id="useremail"
+            :class="{'is-invalid':errors.has('email')}"
+            v-model="form.user.email" placeholder="請輸入 Email" v-validate="'required|email'">
+          <span class="text-danger" v-if="errors.has('email')">{{ errors.first('email') }}</span>
+        </div>
+      
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input type="text" class="form-control" name="name" id="username"
+            :class="{'is-invalid':errors.has('name')}"
+            v-model="form.user.name" placeholder="輸入姓名" v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('name')">姓名欄位不得為空</span>
+        </div>
+      
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input type="tel" class="form-control" id="usertel" name="number"
+          :class="{'is-invalid':errors.has('number')}"
+          v-model="form.user.tel" placeholder="請輸入電話" v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('number')">電話欄位不得為空</span>
+        </div>
+      
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input type="text" class="form-control" name="address" id="useraddress" v-model="form.user.address"
+            :class="{'is-invalid':errors.has('address')}"
+            placeholder="請輸入地址" v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('address')">地址欄位不得留空</span>
+        </div>
+      
+        <div class="form-group">
+          <label for="comment">留言</label>
+          <textarea name="" id="comment" class="form-control" cols="30" rows="10" v-model="form.message"></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -118,13 +169,22 @@ export default {
     return {
       products: [],
       product: {},
-      cart:{},
+      cart:{}, //為何資料結構是物件？因為我們console出來都是response.data.carts
       isLoading: false,
       pagination: {},
       status: {
         loadingItem: ""
       },
       coupon_code:'',
+      form:{
+        user:{
+          name:'',
+          email:'',
+          tel:'',
+          address:'',
+        },
+        message:''
+      }
     };
   },
   methods: {
@@ -189,6 +249,7 @@ export default {
         vm.isLoading = false;
       });
     },
+    //套用優惠碼
     addCouponCode(){
       const vm = this;
       const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/coupon`;
@@ -197,12 +258,34 @@ export default {
       }
       this.$http.post(api,{data:coupon}).then(response => {
         console.log(response.data);
+        if(response.data.success){
+          vm.coupon_code ='';
+          vm.getCart();
+        }else{
+          vm.getCart();
+          console.log('找不到優惠券')
+        }
+      });
+    },
+    // 送出表單
+    sendOrder(){
+      const vm = this;
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/order`;
+      const order = vm.form;
+      this.$validator.validate().then(valid => {
+        if (valid) {
+          this.$http.post(api,{data:order}).then(response => {
+          console.log("訂單已建立", response);
+        });
+        }else{
+          console.log('欄位不完整')
+        }
       });
     },
   },
   created() {
     this.getProducts();
     this.getCart();
-  }
+  },
 };
 </script>

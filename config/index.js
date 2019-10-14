68f91bcb9f55db761aa642598d838db4b16dc30f
chunwen <hsbsbbdn@gmail.com>
2019-10-15 07:49:38 +0800

    調整路徑

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/hexo_vue_demo/',
 
     /**
      * Source Maps
